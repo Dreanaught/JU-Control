f8df0501fef7398992056a15a5b024933432a3b0
Dreanaught <Dreanaught@users.noreply.github.com>
2022-03-07 12:40:10 +0100

    Update const.py

--- custom_components/ju_control/const.py
+++ custom_components/ju_control/const.py
@@ -17,7 +17,11 @@ BINARY_SENSOR_DEVICE_CLASS = "connectivity"
 BINARY_SENSOR = "binary_sensor"
 SENSOR = "sensor"
 SWITCH = "switch"
-PLATFORMS = [BINARY_SENSOR, SENSOR, SWITCH]
+PLATFORMS = [
+  #BINARY_SENSOR, 
+  SENSOR,
+  #SWITCH
+]
 
 
 # Configuration and options
